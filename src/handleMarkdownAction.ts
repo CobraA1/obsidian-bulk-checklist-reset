755bbd1817d63e8baf72fb45c59ba79e3943225c
Luke Hansford <mail@lukehansford.me>
2026-02-27 14:15:10 +0800

    Improve handling of selection ranges
    Although I couldn't recreate it, in theory the selection range Obsidian
    returns might not be in order. I've added some check to handle that
    and potentially resolve https://github.com/lhansford/obsidian-checklist-reset/issues/19.
    In case that's not the solution, I've also added more logging to better
    understand when this error occurs.

--- src/handleMarkdownAction.ts
+++ src/handleMarkdownAction.ts
@@ -1,6 +1,7 @@
 import { EditorSelection, MarkdownView } from "obsidian";
 import { SetAction, setChecklistItems } from "./setChecklistItems";
 import { ChecklistResetSettings } from "./types";
+import { sortPositions } from "./sortPositions";
 
 export function handleMarkdownAction(
   view: MarkdownView,
@@ -8,13 +9,20 @@ export function handleMarkdownAction(
   action: SetAction,
   selectectText?: EditorSelection,
 ) {
-  const currentValue = selectectText ?
-    view.editor.getRange(selectectText.head, selectectText.anchor) :
-    view.getViewData();
-  const newValue = setChecklistItems(currentValue, settings, action);
   if (selectectText) {
-    view.editor.replaceRange(newValue, selectectText.head, selectectText.anchor);
+    const { from, to } = sortPositions(selectectText);
+    const currentValue = view.editor.getRange(from, to);
+    const newValue = setChecklistItems(currentValue, settings, action);
+    try {
+      view.editor.replaceRange(newValue, from, to);
+    } catch (error) {
+      console.log('Selection range: ', { from, to });
+      console.log('Selected text: ', currentValue);
+      console.error(error);
+    }
   } else {
+    const currentValue = view.getViewData();
+    const newValue = setChecklistItems(currentValue, settings, action);
     view.setViewData(newValue, false);
   }
   view.save();
